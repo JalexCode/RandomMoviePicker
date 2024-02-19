9fa48860b41a722df9bfb1fee7fa90c68c35356c
Javier Alejandro González Casellas <78095527+JalexCode@users.noreply.github.com>
2024-02-19 02:02:55 -0500

    feature: add settings and open file, folder buttons

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from sys import argv
 
 from PyQt5 import QtCore
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox
 from qtpy import uic
 
-from data.model import MovieFile
 from feature.movie_collector import MovieCollector
+from helper.settings import add_dirs_to_settings, add_last_movie_to_settings, get_dirs, get_last_movie
+from helper.util import open_file_folder
 from threads.task import Task
 from threads.thread import SubTaskThread
 
@@ -17,16 +18,32 @@ class RandomMoviePicker(QMainWindow):
         QMainWindow.__init__(self)
         uic.loadUi("ui/main.ui", self)
         #
+        self.load_config()
+        #
         self.doItButton.clicked.connect(self.do_it_function)
         self.actionConfigurar.triggered.connect(lambda: self.stackedWidget.setCurrentIndex(0))
         #
         self.movie_collector = MovieCollector()
+        #
+        QMessageBox.warning(self,
+                            "IMPORTANTE",
+                            "LA ÚNICA REGLA PARA USAR ESTE PROGRAMA ES QUE:\nSEA CUAL SEA LA PELÍCULA SELECCIONADA, TIENES QUE VERLA\n¡¡¡ESTA REGLA NO SE PUEDE VIOLAR!!!\n(from me, to myself)")
+
+    def load_config(self):
+        saved_dirs = get_dirs()
+        self.movieFileLineEdit.toPlainText(saved_dirs)
+        #
+        last_movie = get_last_movie()
+        self.set_last_movie(last_movie)
 
     def do_it_function(self):
+        #
         self.movie_collector.del_all()
         #
         dirs_text = self.movieListEditText.toPlainText().strip()
         dirs = dirs_text.split("\n")
+        #
+        add_dirs_to_settings(dirs)
         #
         for dir in dirs:
             self.movie_collector.add_movie_dir(dir)
@@ -46,6 +63,8 @@ class RandomMoviePicker(QMainWindow):
         #
         movie, image = self.movie_collector.pick_random_dir()
         #
+        add_last_movie_to_settings(movie.get_movie_filename())
+        #
         self.movieFileLineEdit.setText("Título:\n" + movie.get_movie_filename())
         self.qualityLabel.setText("Calidad:\n" + movie.get_quality())
         #
@@ -56,8 +75,16 @@ class RandomMoviePicker(QMainWindow):
             # Establecer la imagen en el QLabel
             self.imageLabel.setPixmap(pixmap)
         #
+        self.openFileButton.clicked.connect(lambda: str(open_file_folder(movie.get_folder()) + movie.get_movie_filename()))
+        self.openFolderButton.clicked.connect(lambda: str(open_file_folder(movie.get_folder()) + movie.get_movie_filename()))
+        #
         self.stackedWidget.setCurrentIndex(1)
+        #
+        self.set_last_movie(movie.get_movie_filename(), save=True)
 
+    def set_last_movie(self, last_movie, save=False):
+        if save: add_last_movie_to_settings(last_movie)
+        self.lastMovieLabel.setText("Última selección: " + last_movie)
 def main():
     app = QApplication(argv)
     window = RandomMoviePicker()
